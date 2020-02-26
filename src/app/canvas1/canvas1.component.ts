3b994f8c8739d5abdaaa258d2e31f898d4f83eae
ajdevops1140 <ajdevops1140@gmail.com>
2020-02-26 08:52:22 -0600

    3 files modified, 1 added

--- src/app/canvas1/canvas1.component.ts
+++ src/app/canvas1/canvas1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input} from '@angular/core';
 
 @Component({
   selector: 'app-canvas1',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./canvas1.component.css']
 })
 export class Canvas1Component implements OnInit {
+
+  cvWidth:number = 300;
+  cvHeight:number = 300;
+
 
   constructor() { }
 
