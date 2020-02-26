b18886fb9516cb0de0232bdc7df4547b0b840f2a
ajdevops1140 <ajdevops1140@gmail.com>
2020-02-26 14:51:19 -0600

    1 file modified

--- src/app/canvas1/Basic.ts
+++ src/app/canvas1/Basic.ts
@@ -13,47 +13,23 @@ export class Basic{
 
   draw()
   {
-    // Tangential lines
-    this.ctx.beginPath();
-    this.ctx.strokeStyle = 'gray';
-    this.ctx.moveTo(200, 20);
-    this.ctx.lineTo(200, 130);
-    this.ctx.lineTo(50, 20);
-    this.ctx.stroke();
-
-    // Arc
-    this.ctx.beginPath();
-    this.ctx.strokeStyle = 'black';
-    this.ctx.lineWidth = 5;
-    this.ctx.moveTo(200, 20);
-    this.ctx.arcTo(200,130, 50,20, 40);
-    this.ctx.stroke();
-
-    // Start point
-    this.ctx.beginPath();
-    this.ctx.fillStyle = 'blue';
-    this.ctx.arc(200, 20, 5, 0, 2 * Math.PI);
-    this.ctx.fill();
-
-    // Control points
-    this.ctx.beginPath();
-    this.ctx.fillStyle = 'red';
-    this.ctx.arc(200, 130, 5, 0, 2 * Math.PI); // Control point one
-    this.ctx.arc(50, 20, 5, 0, 2 * Math.PI);   // Control point two
-    this.ctx.fill();
-    /*
     this.ctx.save();
     this.ctx.translate(this.x,this.y);
     this.ctx.rotate(this.r);
     this.ctx.scale(this.sx, this.sy);
     
     this.ctx.beginPath();
-    this.ctx.moveTo(0,0);
-    this.ctx.arcTo(0,20,10,100,300);
-    //this.ctx.lineTo(30,100);
-    this.ctx.moveTo(0,0);
-    this.ctx.lineTo(50,10);
+    //this.ctx.moveTo(0,0);
+    this.ctx.arc(0,0,20,-5* Math.PI/6,-1 *Math.PI/6,true);
+    this.ctx.fillStyle = 'red';    
+    //this.ctx.fillRect(17,-11,2,2);
+    //this.ctx.fillRect(-0,-30,2,2);
+    this.ctx.moveTo(-17,-10);
+    this.ctx.lineTo(-0,-30);
+    this.ctx.moveTo(17,-10);
+    this.ctx.lineTo(-0,-30);
+    this.ctx.closePath();
     this.ctx.stroke();
-    this.ctx.restore();*/
+    this.ctx.restore();
   }
 }
