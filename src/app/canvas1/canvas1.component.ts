5959b568c749b6bf1061521764a4f49b521fc769
ajdevops1140 <ajdevops1140@gmail.com>
2020-02-26 09:41:44 -0600

    1 file modified

--- src/app/canvas1/canvas1.component.ts
+++ src/app/canvas1/canvas1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input} from '@angular/core';
+import { Component, OnInit, ViewChild, Input,ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-canvas1',
@@ -9,11 +9,24 @@ export class Canvas1Component implements OnInit {
 
   cvWidth:number = 300;
   cvHeight:number = 300;
-  @ViewChild('cv',{static:true}) cv:HTMLCanvasElement
+  @ViewChild('cv',{static:true}) cv:ElementRef
+  ctx:CanvasRenderingContext2D
 
-  constructor() { }
+  constructor(){}
 
   ngOnInit() {
+    this.ctx = this.cv.nativeElement.getContext('2d');
+    this.draw();
+  }
+
+  draw()
+  {
+    this.ctx.clearRect(0,0,this.cvWidth,this.cvHeight);
+    this.ctx.beginPath();
+    this.ctx.moveTo(50,50);
+    this.ctx.arc(50,50,20,0,2 * Math.PI,true);
+    this.ctx.stroke();
+    
   }
 
 }
