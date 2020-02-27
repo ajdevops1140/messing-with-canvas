28eaf686da157974db5a68125498d1ffe314614c
ajdevops1140 <ajdevops1140@gmail.com>
2020-02-27 15:00:54 -0600

    1 file modified

--- src/app/util/canvas-handler.service.ts
+++ src/app/util/canvas-handler.service.ts
@@ -36,12 +36,22 @@ export class CanvasHandlerService {
   {
     let p2d:P2D = this.drawArray[index];
     this.ctx.save();
+    this.ctx.fillStyle = 'red';
     this.ctx.translate(this.width,this.height);
-    this.ctx.fillText(`${index}`,p2d.x,p2d.y);
-
-    this.ctx.arc(p2d.x,p2d.y,5,0,2 * Math.PI);
-    
+    this.ctx.fillText(`${index}:(${p2d.x}),(${p2d.y})`,p2d.x + 5,p2d.y + 5);
+    this.ctx.fillStyle = 'blue';
+    this.ctx.arc(p2d.x,p2d.y,5,0,2 * Math.PI);  
+    this.ctx.moveTo(0,0);
+    this.ctx.lineTo(p2d.x,p2d.y);  
     this.ctx.restore();
+  }
+
+  drawPoints()
+  {
+    for(let i = 0; i < this.drawArray;i++)
+    {
+      this.drawPoint(this.ctx,i);
+    }
   }
 
 }
