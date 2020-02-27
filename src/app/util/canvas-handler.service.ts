f8d6973768839810ea5414efe20e184dfae4443c
ajdevops1140 <ajdevops1140@gmail.com>
2020-02-27 14:42:19 -0600

    1 file modified

--- src/app/util/canvas-handler.service.ts
+++ src/app/util/canvas-handler.service.ts
@@ -25,7 +25,28 @@ export class CanvasHandlerService {
 
   removePoint(index:number)
   {
+    this.drawArray.splice(index,1);
+  }
+
+  drawPoints(ctx:CanvasRenderingContext2D, index:number)
+  {
+    let p2d = this.drawArray[index];
+    this.ctx.save();
+    this.ctx.translate(this.x,this.y);
+    this.ctx.rotate(this.r);
+    this.ctx.scale(this.sx, this.sy);
     
+    this.ctx.fillStyle = this.color;
+
+    this.ctx.beginPath();  
+    this.ctx.moveTo(-0,-30);
+    this.ctx.lineTo(-17,-10);
+    this.ctx.arc(0,0,20,-5* Math.PI/6,-1 *Math.PI/6,true);     
+    this.ctx.moveTo(17,-10);
+    this.ctx.lineTo(-0,-30);
+    this.ctx.fill();
+    this.ctx.stroke();
+    this.ctx.restore();
   }
 
 }
