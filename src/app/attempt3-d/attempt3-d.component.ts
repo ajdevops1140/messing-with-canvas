205c89a9e59050af955e6aad5993ff54774a55ce
ajdevops1140 <ajdevops1140@gmail.com>
2020-03-07 09:40:31 -0600

    2 files modified

--- src/app/attempt3-d/attempt3-d.component.ts
+++ src/app/attempt3-d/attempt3-d.component.ts
@@ -6,9 +6,7 @@ import {
   ElementRef,
   AfterViewInit
 } from "@angular/core";
-import { box3D } from '../Objects/box3D';
-import { vec4,vec3 } from '../util/vector';
-import { mat4 } from '../util/mat4';
+
 
 @Component({
   selector: 'app-attempt3-d',
@@ -20,54 +18,23 @@ export class Attempt3DComponent implements OnInit {
   cvWidth: number = 300;
   cvHeight: number = 300;
   @ViewChild("cv", { static: true }) cv: ElementRef;
-  ctx: CanvasRenderingContext2D;
-  wRight: number;
-  wLeft: number;
-  hUp: number;
-  hDown: number;
-  t: number = 0;
-  rotation: number = 0;
-  speed: number = 0.25;
-  tSpeed: number = (1 / 60) * this.speed;
-  w;
-  h;
-  box:box3D;
+  gl: WebGLRenderingContext;
+  
 
   constructor() { }
 
   ngOnInit() {
-    this.ctx = this.cv.nativeElement.getContext("2d");
-    this.w = this.cvWidth / 2;
-    this.h = this.cvHeight / 2;
-    this.wRight = this.w;
-    this.wLeft = -this.w;
-    this.hUp = -this.h;
-    this.hDown = this.h;  
-    this.box = new box3D();  
+  
   }
 
   ngAfterViewInit() {
-    this.box.oX = 0 ;// this.wRight;
-    this.box.oY = 0 ;// this.hDown;
-    this.box.fovX = 80;
-    this.box.fovY = 80;
-    this.box.tX = 20;
-    this.box.ty = 20;
-    this.box.f = 0.8;
-    this.box.n = -0.1;
-    this.box.rotation = 0;
 
-    this.Draw(this.ctx);
   }
 
-  Draw(ctx:CanvasRenderingContext2D)
+  Draw(gl: WebGLRenderingContext)
   {
-    ctx.clearRect(0,0,this.cvWidth,this.cvHeight);
-    this.box.draw(ctx);
-    this.box.rot = 0.001;
-    this.box.axis = 'x';
-
-    window.requestAnimationFrame(()=>this.Draw(ctx));
+    
+    
   }
 
 }
