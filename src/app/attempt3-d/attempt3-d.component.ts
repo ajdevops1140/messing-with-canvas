43cd61ada1810b52f551cc9db27477b56b2656e6
ajdevops1140 <ajdevops1140@gmail.com>
2020-03-07 10:00:13 -0600

    2 files modified

--- src/app/attempt3-d/attempt3-d.component.ts
+++ src/app/attempt3-d/attempt3-d.component.ts
@@ -20,21 +20,55 @@ export class Attempt3DComponent implements OnInit {
   @ViewChild("cv", { static: true }) cv: ElementRef;
   gl: WebGLRenderingContext;
   
+  VSHADER_SOURCE = `void main(){
+    gl_Position = vec4(0.0,0.0,0.0,1.0);
+    gl_PointSize = 10.0;
+  }`;
+
+  FSHADER_SOURCE = `void main(){
+    gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+  }`;
+
+  
 
   constructor() { }
 
   ngOnInit() {
-  
+    this.gl = this.cv.nativeElement.getContext('webgl');
+    console.log(this.VSHADER_SOURCE);
   }
 
   ngAfterViewInit() {
-
+    this.Draw(this.gl);
   }
 
   Draw(gl: WebGLRenderingContext)
   {
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
     
-    
+  }
+
+  initShaders(gl,vshader,fshader)
+  {
+    let program = this.createProgram(gl,vshader,fshader);
+  }
+
+  createProgram(gl,vshader, fshader)
+  {
+    let vertexShader = loadShader(gl, gl.VERTEX_SHADER, vshader);
+  }
+
+  loadShader(gl, type, source)
+  {
+    let shader = gl.createShader(type);     //Create the type of shader - VERTEX
+    if(shader == null)
+    {
+      console.log("unable to create shader");
+      return null;
+    }
+
+
   }
 
 }
