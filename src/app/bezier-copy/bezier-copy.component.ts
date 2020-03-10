0c0bc29fbdb766b695a8d7f153d6812dac857378
ajdevops1140 <ajdevops1140@gmail.com>
2020-03-09 22:51:17 -0500

    1 file modified

--- src/app/bezier-copy/bezier-copy.component.ts
+++ src/app/bezier-copy/bezier-copy.component.ts
@@ -33,8 +33,8 @@ export class BezierCopyComponent implements OnInit {
 
   ngAfterViewInit()
   {   
-    this.c = new Curve(0,0,-30,-100,210,-80,200,0,this.w,this.h);
-    this.c.setSteps(20);
+    this.c = new Curve(0,0,-30,100,210,80,200,0,this.w,this.h);
+    this.c.setSteps(10);
     //this.c.setupPoints();   
     //this.c.setDisplacements();
     
