78651dee4190ac10cd88025c88ef87e44f3de133
ajdevops1140 <ajdevops1140@gmail.com>
2020-03-09 21:46:40 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { Math2DComponent } from './math2-d/math2-d.component';
 import { CanvasHandlerService } from './util/canvas-handler.service';
 import { Interpolate2PointsComponent } from './interpolate2-points/interpolate2-points.component';
 import { BezierComponent } from './bezier/bezier.component';
+import { BezierCopyComponent } from './bezier-copy/bezier-copy.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, Canvas1Component, Math2DComponent, Interpolate2PointsComponent, BezierComponent],
+  declarations: [ AppComponent, HelloComponent, Canvas1Component, Math2DComponent, Interpolate2PointsComponent, BezierComponent, BezierCopyComponent],
   bootstrap:    [ AppComponent ],
   providers: [CanvasHandlerService]
 })
---
+++ src/app/bezier-copy/bezier-copy.component.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
+import { P2v } from "../util/P2DCopy";
+import { Curve } from "../util/Curve";
+
+@Component({
+  selector: 'app-bezier-copy',
+  templateUrl: './bezier-copy.component.html',
+  styleUrls: ['./bezier-copy.component.css']
+})
+export class BezierCopyComponent implements OnInit {
+  cvWidth: number = 800;
+  cvHeight: number = 800;
+  @ViewChild("cv", { static: true }) cv: ElementRef;
+  ctx: CanvasRenderingContext2D;
+  c:Curve;
+  w:number;
+  h:number;
+
+  constructor() { }
+
+  ngOnInit() {
+     this.ctx = this.cv.nativeElement.getContext('2d'); 
+     this.w = this.cvWidth/2;
+     this.h = this.cvHeight/2;
+  }
+
+  ngAfterViewInit()
+  {   
+    this.c = new Curve(0,200,0,0,0,0,200,200,this.w,this.h);
+    this.c.setSteps(5);
+    this.c.setupPoints();   
+    this.c.setDisplacements();
+    
+    
+    this.draw(this.ctx);
+  }
+
+  draw(ctx:CanvasRenderingContext2D)
+  {
+    ctx.clearRect(0,0,this.cvWidth,this.cvHeight);
+    this.c.drawDisplacement(this.ctx);   
+    this.c.tX = 100; 
+    this.c.drawFromSteps(this.ctx);
+    this.c.rot += 0.0;
+    this.c.tX += 0.0;
+    //window.requestAnimationFrame(()=>{this.draw(ctx)});
+  }
+
+}
