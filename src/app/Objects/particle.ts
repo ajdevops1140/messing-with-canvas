807c59f9657b6400578d35ec064a62815fed9345
ajdevops1140 <ajdevops1140@gmail.com>
2020-03-12 23:10:44 -0500

    1 file modified

--- src/app/Objects/particle.ts
+++ src/app/Objects/particle.ts
@@ -1,5 +1,6 @@
 import { P2v } from '../util/P2DCopy';
 import { base2D } from './base2D';
+import { Curve } from '../util/CurveCopy';
 
 
 export class Particle extends base2D
@@ -13,7 +14,7 @@ export class Particle extends base2D
     t:number;
     rotation:number;
   */
-  
+  curve:Curve;
    
    constructor(oX:number = 0,oY:number = 0,size = 6)
    {
